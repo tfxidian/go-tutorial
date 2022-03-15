7ad47b738822951c90f26504a1a67ddab54f0a17
tfxidian <wanmeibaidu@163.com>
2022-03-15 15:02:06 +0800

    add env detect

---
+++ env detect/DumpCgroup.go
@@ -0,0 +1,28 @@
+
+package  main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"bufio"
+	"strings"
+)
+
+var MainPIDCgroup = "/proc/1/cgroup"
+
+func main() {
+
+	data, err := ioutil.ReadFile(MainPIDCgroup)
+	if err != nil {
+		log.Printf("err found while open %s: %v\n", MainPIDCgroup, err)
+		return
+	}
+	log.Println("/proc/1/cgroup file content:")
+
+	scanner := bufio.NewScanner(strings.NewReader(string(data)))
+	for scanner.Scan() {
+		fmt.Printf("\t%s\n", scanner.Text())
+	}
+
+}
